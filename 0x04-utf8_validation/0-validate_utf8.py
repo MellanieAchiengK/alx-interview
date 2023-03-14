fecbfce68449677b48ea85b2200882979a47146f
Mellanie <mellanie.achieng@gmail.com>
2023-03-14 13:02:01 +0000

    style fix

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """determines if a given data set represents a valid UTF-8 encoding"""
+
+
 def validUTF8(data):
     """data"""
     # Number of bytes in the current UTF-8 character
