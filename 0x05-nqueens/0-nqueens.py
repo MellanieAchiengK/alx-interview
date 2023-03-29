e1a466e698d668fe5f8a6d78d228afdd814f46d4
Mellanie <mellanie.achieng@gmail.com>
2023-03-29 09:46:12 +0000

    code fix

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,58 +1,43 @@
 #!/usr/bin/python3
-"""N queens puzzle challenge"""
+'''
+Module that solves the N Queens puzzle
+'''
+from sys import argv, exit
 
-
-def print_solutions(queens, n):
-    """
-    Print all solutions to N Queens problem
-
-    :param queens: a list of size N that stores the position of queens 
-    in the current solution
-    :param n: the number of rows and columns on the chessboard
-    """
-
-    # If all queens are placed, print the solution
-    if len(queens) == n:
-        print([[i, queens[i]] for i in range(n)])
-        return
-
-    # Check every row in the current column for a valid position
-    for i in range(n):
-        # Assume the current position is valid
-        valid = True
-
-        # Check for any queens in the same row or diagonal
-        for j in range(len(queens)):
-            if queens[j] == i or queens[j] - j == i - len(queens) or queens[j] + j == i + len(queens):
-                valid = False
-                break
-
-        # If the current position is valid, add it to the list 
-        # and continue to the next column
-        if valid:
-            queens.append(i)
-            print_solutions(queens, n)
-            queens.pop()
-
-# Driver code to parse command line argument and call print_solutions
-if __name__ == '__main__':
-    import sys
-
-    # Check if correct number of arguments are provided
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print('Usage: nqueens N')
+        exit(1)
     try:
-        n = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-
-    # Check if n is valid
+        n = int(argv[1])
+    except BaseException:
+        print('N must be a number')
+        exit(1)
     if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
+        print('N must be at least 4')
+        exit(1)
 
-    # Call print_solutions with an empty list of queens and n
-    print_solutions([], n)
+    solution = []
+
+    def solve_queens(row, n, solution):
+        if (row == n):
+            print(solution)
+        else:
+            for col in range(n):
+                placement = [row, col]
+                if valid_placement(solution, placement):
+                    solution.append(placement)
+                    solve_queens(row + 1, n, solution)
+                    solution.remove(placement)
+
+    def valid_placement(solution, placement):
+        for queen in solution:
+            if queen[1] == placement[1]:
+                return False
+            if (queen[0] + queen[1]) == (placement[0] + placement[1]):
+                return False
+            if (queen[0] - queen[1]) == (placement[0] - placement[1]):
+                return False
+        return True
+
+    solve_queens(0, n, solution)
